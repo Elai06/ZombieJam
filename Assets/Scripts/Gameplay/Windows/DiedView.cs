2c2b828320fa63cd2c2ad8beabe35b1bac3c0630
Elai <dof312@gmail.com>
2024-02-05 18:23:51 +0300

    DiedView

--- Assets/Scripts/Gameplay/Windows/DiedView.cs
+++ Assets/Scripts/Gameplay/Windows/DiedView.cs
@@ -1,4 +1,5 @@
 ﻿using Gameplay.Enums;
+using Gameplay.Level;
 using Gameplay.Windows.Gameplay;
 using TMPro;
 using UnityEngine;
@@ -13,9 +14,13 @@ namespace Gameplay.Windows
         [SerializeField] private TextMeshProUGUI _waveText;
         [SerializeField] private TextMeshProUGUI _regionName;
         [SerializeField] private TextMeshProUGUI _experienceText;
+        [SerializeField] private TextMeshProUGUI _levelText;
+        [SerializeField] private TextMeshProUGUI _currentExperienceSlider;
+        [SerializeField] private Slider _levelSlider;
         [SerializeField] private Button _ressurectionButton;
 
         [Inject] private IGameplayModel _gameplayModel;
+        [Inject] private ILevelModel _levelModel;
 
         public void Start()
         {
@@ -28,7 +33,7 @@ namespace Gameplay.Windows
             var waveIndex = progress.CurrentWaweIndex == 0 ? 0 : progress.CurrentWaweIndex - 1;
             SetWave(progress.ERegionType, waveIndex);
             _gameplayModel.LooseWave();
-            _experienceText.text = $"Experience: +{_gameplayModel.GetExperience(false)}";
+            SetLevel();
 
             if (_gameplayModel.WaveType == EWaveType.Logic)
             {
@@ -36,24 +41,38 @@ namespace Gameplay.Windows
                 return;
             }
             
-            _ressurectionButton.onClick.AddListener(Ressurection);
+            _ressurectionButton.onClick.AddListener(Revive);
             _ressurectionButton.gameObject.SetActive(_gameplayModel.IsAvailableRessuraction);
         }
 
-        private void Ressurection()
+        private void Revive()
         {
-            _gameplayModel.RessurectionUnits();
+            if (_gameplayModel.IsAvailableRessuraction)
+            {
+                _gameplayModel.RessurectionUnits();
+            }
         }
 
         private void OnDisable()
         {
-            _ressurectionButton.onClick.AddListener(Ressurection);
+            _ressurectionButton.onClick.AddListener(Revive);
         }
 
         private void SetWave(ERegionType regionType, int index)
         {
-            _waveText.text = $"Волна {index}";
+            _waveText.text = $"Wave {index + 1} loose";
             _regionName.text = regionType.ToString();
+        }
+
+        private void SetLevel()
+        {
+            var currentLevel = _levelModel.CurrentLevel;
+            var reqiredExperience = _levelModel.ReqiredExperienceForUp();
+            var currentExperience = _levelModel.CurrentExperience;
+            _experienceText.text = $" +{_gameplayModel.GetExperience(true)} experience";
+            _levelText.text = $"{currentLevel + 1}";
+            _levelSlider.value = (float)currentExperience / reqiredExperience;
+            _currentExperienceSlider.text = $"{currentExperience}/{reqiredExperience}";
         }
     }
 }
