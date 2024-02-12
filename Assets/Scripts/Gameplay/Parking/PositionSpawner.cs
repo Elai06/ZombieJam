06c991cee43bbd50e921d09f458d87a64ce17647
Elai <dof312@gmail.com>
2024-02-12 13:11:10 +0300

    Corrected visual in waves

--- Assets/Scripts/Gameplay/Parking/PositionSpawner.cs
+++ Assets/Scripts/Gameplay/Parking/PositionSpawner.cs
@@ -72,6 +72,7 @@ namespace Gameplay.Parking
             for (int i = 0; i < _spawnGrid.Count; i++)
             {
                 var spawnPosition = _spawnGrid[i];
+                DestroyImmediate(spawnPosition.CubePrefab);
                 if (spawnPosition.CubePrefab == null)
                 {
                     _spawnGrid[i].SetCubePrefab(Instantiate(_cubePrefab, Vector3.zero, Quaternion.identity,
