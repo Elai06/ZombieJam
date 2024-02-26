e1f7bebf02836bae9ca0361c35b675dc2ad92a8a
Elai <dof312@gmail.com>
2024-02-26 14:59:26 +0300

    Added slider waves 4->8 in lobby View

--- Assets/Scripts/Gameplay/Windows/Lobby/LobbyView.cs
+++ Assets/Scripts/Gameplay/Windows/Lobby/LobbyView.cs
@@ -12,15 +12,12 @@ namespace Gameplay.Windows.Lobby
 {
     public class LobbyView : MonoBehaviour
     {
-        [SerializeField] private Slider _slider;
+        [SerializeField] private WaveSlider _wave4Slider;
+        [SerializeField] private WaveSlider _wave6Slider;
+        [SerializeField] private WaveSlider _wave8Slider;
+        
         [SerializeField] private TextMeshProUGUI _regionText;
         [SerializeField] private TextMeshProUGUI _waveText;
-
-        [SerializeField] private Sprite _completedWave;
-        [SerializeField] private Sprite _notCompletedWave;
-        [SerializeField] private Sprite _currentWave;
-
-        [SerializeField] private List<Image> _waveSprites;
 
         [Inject] private IWindowService _windowService;
         [Inject] private IGameplayModel _gameplayModel;
@@ -32,6 +29,7 @@ namespace Gameplay.Windows.Lobby
             _waveText.gameObject.SetActive(false);
 
             var progress = _gameplayModel.GetCurrentRegionProgress().GetCurrentRegion();
+            GetActualSlider().gameObject.SetActive(true);
             UpdateWave(progress.ERegionType, progress.CurrentWaweIndex);
             _gameplayModel.OnToTheNextWave += UpdateWave;
             _windowService.OnOpen += OpenWindow;
@@ -39,6 +37,11 @@ namespace Gameplay.Windows.Lobby
 
         private void OnEnable()
         {
+            _wave4Slider.gameObject.SetActive(false);
+            _wave6Slider.gameObject.SetActive(false);
+            _wave8Slider.gameObject.SetActive(false);
+            
+
             if (_windowService != null)
             {
                 if (_isFirstOpen)
@@ -54,37 +57,21 @@ namespace Gameplay.Windows.Lobby
             if (_gameplayModel != null)
             {
                 var progress = _gameplayModel.GetCurrentRegionProgress().GetCurrentRegion();
+                GetActualSlider().gameObject.SetActive(true);
                 UpdateWave(progress.ERegionType, progress.CurrentWaweIndex);
             }
         }
 
         private void UpdateWave(ERegionType regionType, int waveIndex)
         {
-            var config = _gameplayModel.GetRegionConfig();
-            _slider.value = waveIndex / ((float)config.Waves.Count - 1);
+            var config = _gameplayModel.GetRegionConfig().Waves;
+            
+            var slider = GetActualSlider();
+            
+            slider.UpdateWave(config.Count, waveIndex);
+            
             _regionText.text = $"{regionType}";
             _waveText.text = $"Wave {waveIndex + 1}";
-            UpdateSliderWavesImage(waveIndex);
-        }
-
-        private void UpdateSliderWavesImage(int waveIndex)
-        {
-            for (var index = 0; index < _waveSprites.Count; index++)
-            {
-                var waveSprite = _waveSprites[index];
-                if (waveIndex > index)
-                {
-                    waveSprite.sprite = _completedWave;
-                }
-                else if (waveIndex == index)
-                {
-                    waveSprite.sprite = _currentWave;
-                }
-                else
-                {
-                    waveSprite.sprite = _notCompletedWave;
-                }
-            }
         }
 
         private void Restart()
@@ -105,17 +92,32 @@ namespace Gameplay.Windows.Lobby
             _windowService.Open(WindowType.Footer);
         }
 
+        private WaveSlider GetActualSlider()
+        {
+            var waves = _gameplayModel.GetRegionConfig().Waves;
+
+            return waves.Count switch
+            {
+                8 => _wave8Slider,
+                6 => _wave6Slider,
+                4 => _wave4Slider,
+                _ => null
+            };
+        }
+        
         private void OpenWindow(WindowType windowType)
         {
             switch (windowType)
             {
                 case WindowType.Gameplay:
                     _waveText.gameObject.SetActive(true);
-                    _slider.gameObject.SetActive(false);
+                    _wave4Slider.gameObject.SetActive(false);
+                    _wave6Slider.gameObject.SetActive(false);
+                    _wave8Slider.gameObject.SetActive(false);
                     _regionText.gameObject.SetActive(false);
                     break;
                 case WindowType.MainMenu:
-                    _slider.gameObject.SetActive(true);
+                   GetActualSlider().gameObject.SetActive(true);
                     _regionText.gameObject.SetActive(true);
                     _waveText.gameObject.SetActive(false);
                     break;
---
+++ Assets/Scripts/Gameplay/Windows/Lobby/WaveSlider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.Windows.Lobby
+{
+    public class WaveSlider : MonoBehaviour
+    {
+        [SerializeField] private Slider _slider;
+        [SerializeField] private Sprite _completedWave;
+        [SerializeField] private Sprite _notCompletedWave;
+        [SerializeField] private Sprite _currentWave;
+
+        [SerializeField] private List<Image> _waveSprites;
+
+
+        public void UpdateWave(float waveCount, int waveIndex)
+        {
+            _slider.value = waveIndex / (waveCount - 1);
+            UpdateSliderWavesImage(waveIndex);
+        }
+
+        private void UpdateSliderWavesImage(int waveIndex)
+        {
+            for (var index = 0; index < _waveSprites.Count; index++)
+            {
+                var waveSprite = _waveSprites[index];
+                if (waveIndex > index)
+                {
+                    waveSprite.sprite = _completedWave;
+                }
+                else if (waveIndex == index)
+                {
+                    waveSprite.sprite = _currentWave;
+                }
+                else
+                {
+                    waveSprite.sprite = _notCompletedWave;
+                }
+            }
+        }
+    }
+}
